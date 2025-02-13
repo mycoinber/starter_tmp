9ab3ba255e9f1970bf665729727086b517e73242
ihor.filkin <ihor.filkin@boring.agency>
2025-02-13 18:21:08 +0200

    add

--- components/Main/Table.vue
+++ components/Main/Table.vue
@@ -1,77 +1,76 @@
 <script setup>
-  import { ref, useSSRContext, onMounted, useCssModule } from "vue";
-  import { parse } from "node-html-parser";
+import { ref, useSSRContext, onMounted, useCssModule } from "vue";
+import { parse } from "node-html-parser";
 
-  const styles = useCssModule();
+const styles = useCssModule();
 
-  const props = defineProps({
-    data: {
-      type: Object,
-      default: () => ({}),
-    },
-  });
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
+});
 
-  const ssrContext = import.meta.server ? useSSRContext() : null;
-  const contentHtml = ref("");
+const ssrContext = import.meta.server ? useSSRContext() : null;
+const contentHtml = ref("");
 
-  const parseHTML = (html) => {
-    if (import.meta.server) {
-      return parse(html);
-    } else {
-      const parser = new DOMParser();
-      return parser.parseFromString(html, "text/html");
-    }
-  };
+const parseHTML = (html) => {
+  if (import.meta.server) {
+    return parse(html);
+  } else {
+    const parser = new DOMParser();
+    return parser.parseFromString(html, "text/html");
+  }
+};
 
-  const processHtmlContent = (htmlString) => {
-    const doc = parseHTML(htmlString);
+const processHtmlContent = (htmlString) => {
+  const doc = parseHTML(htmlString);
 
-    // Функция для добавления классов к элементам
-    const addClasses = (selector, styleClass) => {
-      const elements = doc.querySelectorAll(selector);
-      elements.forEach((el) => {
-        if (import.meta.server) {
-          const classAttr = el.getAttribute("class") || "";
-          el.setAttribute("class", `${classAttr} ${styles[styleClass]}`);
-        } else {
-          el.classList.add(styles[styleClass]);
-        }
-      });
-    };
-
-    // Оборачиваем таблицу в дополнительный div
-    const table = doc.querySelector("table");
-    if (table) {
+  // Функция для добавления классов к элементам
+  const addClasses = (selector, styleClass) => {
+    const elements = doc.querySelectorAll(selector);
+    elements.forEach((el) => {
       if (import.meta.server) {
-        // Серверный рендеринг: оборачиваем таблицу с помощью строковой модификации
-        const wrapperHtml = `<div class="${
-          styles.tableWrapper
-        }">${table.toString()}</div>`;
-        table.replaceWith(parse(wrapperHtml));
+        const classAttr = el.getAttribute("class") || "";
+        el.setAttribute("class", `${classAttr} ${styles[styleClass]}`);
       } else {
-        // Клиентский рендеринг: создаем DOM-элемент
-        const wrapper = document.createElement("div");
-        wrapper.className = styles.tableWrapper;
-        table.parentNode.insertBefore(wrapper, table);
-        wrapper.appendChild(table);
+        el.classList.add(styles[styleClass]);
       }
-    }
-
-    return import.meta.server ? doc.toString() : doc.body.innerHTML;
+    });
   };
 
-  if (import.meta.server && props.data.type === "table") {
-    const modifiedHtml = processHtmlContent(props.data.content);
-    ssrContext.modifiedHtml = modifiedHtml;
-    contentHtml.value = modifiedHtml;
+  // Оборачиваем таблицу в дополнительный div
+  const table = doc.querySelector("table");
+  if (table) {
+    if (import.meta.server) {
+      // Серверный рендеринг: оборачиваем таблицу с помощью строковой модификации
+      const wrapperHtml = `<div class="${styles.tableWrapper
+        }">${table.toString()}</div>`;
+      table.replaceWith(parse(wrapperHtml));
+    } else {
+      // Клиентский рендеринг: создаем DOM-элемент
+      const wrapper = document.createElement("div");
+      wrapper.className = styles.tableWrapper;
+      table.parentNode.insertBefore(wrapper, table);
+      wrapper.appendChild(table);
+    }
   }
 
-  onMounted(() => {
-    if (props.data.type === "table") {
-      contentHtml.value =
-        ssrContext?.modifiedHtml || processHtmlContent(props.data.content);
-    }
-  });
+  return import.meta.server ? doc.toString() : doc.body.innerHTML;
+};
+
+if (import.meta.server && props.data.type === "table") {
+  const modifiedHtml = processHtmlContent(props.data.content);
+  ssrContext.modifiedHtml = modifiedHtml;
+  contentHtml.value = modifiedHtml;
+}
+
+onMounted(() => {
+  if (props.data.type === "table") {
+    contentHtml.value =
+      ssrContext?.modifiedHtml || processHtmlContent(props.data.content);
+  }
+});
 </script>
 
 <template>
@@ -82,20 +81,27 @@
   </section>
 </template>
 
-<style lang="scss" scoped module>
-  .block {
-    margin: 2rem 0;
+<style lang="scss" module>
+.block {
+  margin: 2rem 0;
 
-    @include media(mobile) {
-      margin: 1rem 0;
-    }
+  @include media(mobile) {
+    margin: 1rem 0;
   }
+}
 
-  .wrapper {
-    overflow: hidden;
-  }
+.wrapper {
+  overflow: hidden;
+}
 
-  .tableWrapper {
-    overflow-x: auto;
+.tableWrapper {
+  @include media(mobile) {
+    min-width: 100%;
+    width: 100%;
+    overflow-y: hidden;
+    overflow-x: scroll;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
   }
+}
 </style>
