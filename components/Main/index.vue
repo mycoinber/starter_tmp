7f5b0a5839fc62b44d893907499cd2275b1a97e6
ivan <berezhnuy.ivan@gmail.com>
2025-04-03 20:23:22 +0300

    minus

--- components/Main/index.vue
+++ components/Main/index.vue
@@ -1,61 +1,61 @@
 <script setup>
-import { useCssModule } from "vue";
+  import { useCssModule } from "vue";
 
-const styles = useCssModule();
+  const styles = useCssModule();
 
-const props = defineProps({
-  data: {
-    type: Object,
-    default: () => ({}),
-  },
-});
-
-const faqs = computed(() => {
-  return (
-    props.data.article.blocks?.find(
-      (item) => item.faqs && Array.isArray(item.faqs) && item.faqs.length > 0
-    ) || null
-  );
-});
-
-// Возвращаем первый блок с непустым reviews
-const reviews = computed(() => {
-  return (
-    props.data.article.blocks?.find(
-      (item) =>
-        item.reviews && Array.isArray(item.reviews) && item.reviews.length > 0
-    ) || null
-  );
-});
-
-const sections = computed(() => {
-  return (
-    props.data.article.blocks?.filter(
-      (item) =>
-        !(
-          (item.faqs && Array.isArray(item.faqs) && item.faqs.length > 0) ||
-          (item.reviews &&
-            Array.isArray(item.reviews) &&
-            item.reviews.length > 0)
-        )
-    ) || []
-  );
-});
-
-console.log("sections", sections);
-const isLoaded = ref(false);
-const isBot = useState("isBot", () => false);
-
-if (import.meta.server) {
-  const event = useRequestEvent();
-  isBot.value = event.context.isBot || false;
-} else {
-  onMounted(() => {
-    setTimeout(() => {
-      isLoaded.value = true;
-    }, 100);
+  const props = defineProps({
+    data: {
+      type: Object,
+      default: () => ({}),
+    },
   });
-}
+
+  const faqs = computed(() => {
+    return (
+      props.data.article.blocks?.find(
+        (item) => item.faqs && Array.isArray(item.faqs) && item.faqs.length > 0
+      ) || null
+    );
+  });
+
+  // Возвращаем первый блок с непустым reviews
+  const reviews = computed(() => {
+    return (
+      props.data.article.blocks?.find(
+        (item) =>
+          item.reviews && Array.isArray(item.reviews) && item.reviews.length > 0
+      ) || null
+    );
+  });
+
+  const sections = computed(() => {
+    return (
+      props.data.article.blocks?.filter(
+        (item) =>
+          !(
+            (item.faqs && Array.isArray(item.faqs) && item.faqs.length > 0) ||
+            (item.reviews &&
+              Array.isArray(item.reviews) &&
+              item.reviews.length > 0)
+          )
+      ) || []
+    );
+  });
+
+  console.log("sections", sections);
+  const isLoaded = ref(false);
+  const isBot = useState("isBot", () => false);
+
+  if (import.meta.server) {
+    const event = useRequestEvent();
+    isBot.value = event.context.isBot || false;
+  } else {
+    onMounted(() => {
+      setTimeout(() => {
+        isLoaded.value = true;
+      }, 100);
+    });
+  }
 </script>
 
 <template>
@@ -63,15 +63,21 @@ if (import.meta.server) {
     <MainLoader />
   </div>
 
-  <section :class="styles.block">
+  <section :class="[styles.block, data.offer ? styles.offer : '']">
     <div class="container">
       <DelayHydration>
         <MainHero v-if="!isBot" :data="data" />
       </DelayHydration>
 
       <div :class="styles.img">
-        <NuxtImg :src="`unsplash${data.hero[0]?.path}`" :alt="data.hero[0]?.title" width="800" loading="lazy"
-          quality="75" sizes="xs:100vw sm:100vw md:50vw lg:50vw xl:33vw" />
+        <NuxtImg
+          :src="`unsplash${data.hero[0]?.path}`"
+          :alt="data.hero[0]?.title"
+          width="800"
+          loading="lazy"
+          quality="75"
+          sizes="xs:100vw sm:100vw md:50vw lg:50vw xl:33vw"
+        />
       </div>
     </div>
   </section>
@@ -89,63 +95,69 @@ if (import.meta.server) {
 </template>
 
 <style lang="scss" scoped module>
-.loaderContainer {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--background-01);
-  z-index: 9999;
-}
-
-.block {
-  width: 100%;
-  height: 65rem;
-  position: relative;
-  z-index: 2;
-  margin-bottom: 4rem;
-
-  @include media(mobile) {
-    min-height: 120vh;
-    height: fit-content;
-  }
-
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
+  .loaderContainer {
+    position: fixed;
+    top: 0;
     left: 0;
     width: 100%;
-    height: 70%;
-    z-index: -1;
-    background: linear-gradient(to bottom,
-        rgba(255, 255, 255, 0),
-        var(--background-01));
-    pointer-events: none;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--background-01);
+    z-index: 9999;
   }
-}
 
-.img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -2;
-
-  img {
+  .block {
     width: 100%;
-    height: 100%;
-    object-fit: cover;
+    height: 65rem;
+    position: relative;
+    z-index: 2;
+    margin-bottom: 4rem;
 
     @include media(mobile) {
-      object-fit: contain;
-      object-position: top center;
+      min-height: 120vh;
+      height: fit-content;
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 70%;
+      z-index: -1;
+      background: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 0),
+        var(--background-01)
+      );
+      pointer-events: none;
     }
   }
-}
+
+  .img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+
+      @include media(mobile) {
+        object-fit: contain;
+        object-position: top center;
+      }
+    }
+  }
+
+  .offer {
+    font-size: large;
+  }
 </style>
